d0a076fed1d16cde2c8aa698d65f72965fdd62ea
aminsato <amin.saradar@yahoo.com>
2022-03-20 10:43:32 +0330

    commit

--- app/src/main/java/io/android/fanito_androidclient/ui/local/AppPreferencesHelper.kt
+++ app/src/main/java/io/android/fanito_androidclient/ui/local/AppPreferencesHelper.kt
@@ -1,4 +1,55 @@
 package io.android.fanito_androidclient.ui.local
 
-class AppPreferencesHelper {
+import android.content.Context
+import android.content.SharedPreferences
+import java.util.Date
+
+class AppPreferencesHelper @Inject constructor(
+    context: Context,
+    @PreferenceInfo prefFileName: String?
+) : PreferencesHelper {
+
+    private val mPrefs: SharedPreferences =
+        context.getSharedPreferences(prefFileName, Context.MODE_PRIVATE)
+
+    override fun clearUserData() = mPrefs.edit().clear().apply()
+
+    override var refreshToken: String?
+        get() = mPrefs.getString(PREF_KEY_REFRESH_TOKEN, null)
+        set(value) {
+            mPrefs.edit().putString(PREF_KEY_REFRESH_TOKEN, value).apply()
+        }
+
+
+
+
+    override var accessToken: String?
+        get()
+        {
+            mPrefs.getString(PREF_KEY_ACCESS_TOKEN, null)?.let{
+                var jwt: JWT = JWT(it)
+                var ExpireDate= jwt.expiresAt
+                if(Date()?.before(ExpireDate))
+                {
+
+                }
+            }
+
+            return mPrefs.getString(PREF_KEY_ACCESS_TOKEN, null)
+        }
+        set(value) {
+            mPrefs.edit().putString(PREF_KEY_ACCESS_TOKEN, value).apply()
+        }
+
+    override var userId: String?
+        get() = mPrefs.getString(PREF_KEY_USER_ID, null)
+        set(value) {
+            mPrefs.edit().putString(PREF_KEY_USER_ID, value).apply()
+        }
+
+    companion object {
+        private const val PREF_KEY_REFRESH_TOKEN = "PREF_KEY_REFRESH_TOKEN"
+        private const val PREF_KEY_ACCESS_TOKEN = "PREF_KEY_ACCESS_TOKEN"
+        private const val PREF_KEY_USER_ID = "PREF_KEY_USER_ID"
+    }
 }
