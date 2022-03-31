42d49bc9dca5b4a7c9505ab67e7356848d136ee5
aminsato <amin.saradar@yahoo.com>
2022-03-31 11:06:01 +0330

    complete respone

---
+++ app/src/main/java/io/android/fanito_androidclient/date/model/api/response/PollOptionsResponse.kt
@@ -0,0 +1,40 @@
+package io.android.fanito_androidclient.date.model.api.response
+
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import io.fanito.android.utils.IComparable
+
+data class PollOptionsResponse(
+    @Expose @SerializedName("totalCount") val totalCount: Int?,
+    @Expose @SerializedName("items") val items: List<Option>?,
+) {
+
+    data class Option(
+        @Expose @SerializedName("id") val id: Int,
+        @Expose @SerializedName("pictureURL") val pictureURL: String?,
+        @Expose @SerializedName("title") val title: String?,
+        @Expose @SerializedName("description") val description: String?,
+        @Expose @SerializedName("count") val count: Int?,
+        @Expose @SerializedName("tokens") val tokens: Int?,
+        @Expose @SerializedName("weight") val weight: Int?,
+        var isSelected: Boolean,
+        var isVoted: Boolean,
+        var isSending: Boolean,
+    ) : IComparable {
+
+        override fun comparator() = id
+
+        fun changeSelectState(isSelected: Boolean) {
+            this.isSelected = isSelected
+        }
+
+        fun changeVoteState(isVoted: Boolean) {
+            this.isVoted = isVoted
+        }
+
+        fun changeSendingState(isSending: Boolean) {
+            this.isSending = isSending
+        }
+    }
+}
