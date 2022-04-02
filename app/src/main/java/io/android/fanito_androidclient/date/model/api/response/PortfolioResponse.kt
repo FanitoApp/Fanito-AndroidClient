3644eaa886e6ef9fb770dfae9af92d44e8536b4e
aminsato <amin.saradar@yahoo.com>
2022-04-02 11:07:07 +0330

    Create PortfolioResponse.kt

---
+++ app/src/main/java/io/android/fanito_androidclient/date/model/api/response/PortfolioResponse.kt
@@ -0,0 +1,18 @@
+package io.android.fanito_androidclient.date.model.api.response
+
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import io.fanito.android.utils.IComparable
+
+data class PortfolioResponse(
+    @Expose @SerializedName("description") val description: String?,
+    @Expose @SerializedName("logoURL") val logoURL: String?,
+    @Expose @SerializedName("title") val title: String?,
+    @Expose @SerializedName("price") val price: Long?,
+    @Expose @SerializedName("symbol") val symbol: String?,
+    @Expose @SerializedName("balance") val balance: Long?,
+) : IComparable {
+
+    override fun comparator() = symbol
+}
