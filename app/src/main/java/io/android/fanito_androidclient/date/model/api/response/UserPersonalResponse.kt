91ed0aee7bf4a0f58f5a44a80f76311d7da97ed5
aminsato <amin.saradar@yahoo.com>
2022-04-04 11:09:14 +0330

    Create UserPersonalResponse.kt

---
+++ app/src/main/java/io/android/fanito_androidclient/date/model/api/response/UserPersonalResponse.kt
@@ -0,0 +1,13 @@
+package io.android.fanito_androidclient.date.model.api.response
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class UserPersonalResponse(
+    @Expose @SerializedName("phone") val phone: String?,
+    @Expose @SerializedName("firstName") val firstName: String?,
+    @Expose @SerializedName("lastName") val lastName: String?,
+    @Expose @SerializedName("nationalId") val nationalId: String?,
+    @Expose @SerializedName("birthDate") val birthDate: String?,
+    @Expose @SerializedName("createdAt") val createdAt: String?,
+)
