e384b3d2edc3e332041a9c8bfef02d749bbd1e0f
aminsato <amin.saradar@yahoo.com>
2022-04-05 11:09:46 +0330

    Create VerifyOtpResponse.kt

---
+++ app/src/main/java/io/android/fanito_androidclient/date/model/api/response/VerifyOtpResponse.kt
@@ -0,0 +1,10 @@
+package io.android.fanito_androidclient.date.model.api.response
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class VerifyOtpResponse(
+    @Expose @SerializedName("sessionId") val sessionId: String,
+    @Expose @SerializedName("registered") val registered: Boolean,
+    @Expose @SerializedName("expiresAt") val expiresAt: Long,
+)
