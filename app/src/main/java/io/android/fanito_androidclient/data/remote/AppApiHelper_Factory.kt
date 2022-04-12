b6c874707b63014b54819b342296fbb9dd03b179
aminsato <amin.saradar@yahoo.com>
2022-04-12 11:24:26 +0330

    Create AppApiHelper_Factory.kt

---
+++ app/src/main/java/io/android/fanito_androidclient/data/remote/AppApiHelper_Factory.kt
@@ -0,0 +1,32 @@
+package io.android.fanito_androidclient.data.remote
+
+
+import dagger.internal.DaggerGenerated;
+import dagger.internal.Factory;
+import javax.inject.Provider;
+
+@DaggerGenerated
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes"
+})
+public final class AppApiHelper_Factory implements Factory<AppApiHelper> {
+    private final Provider<ApiServices> apiServicesProvider;
+
+    public AppApiHelper_Factory(Provider<ApiServices> apiServicesProvider) {
+        this.apiServicesProvider = apiServicesProvider;
+    }
+
+    @Override
+    public AppApiHelper get() {
+        return newInstance(apiServicesProvider.get());
+    }
+
+    public static AppApiHelper_Factory create(Provider<ApiServices> apiServicesProvider) {
+        return new AppApiHelper_Factory(apiServicesProvider);
+    }
+
+    public static AppApiHelper newInstance(ApiServices apiServices) {
+        return new AppApiHelper(apiServices);
+    }
+}
