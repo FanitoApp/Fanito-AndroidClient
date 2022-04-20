5e5b8729b63d21e7d996438c6ff5cd6a40abd317
aminsato <amin.saradar@yahoo.com>
2022-04-20 12:17:54 +0330

    Create AppModule.kt

---
+++ app/src/main/java/io/android/fanito_androidclient/di/AppModule.kt
@@ -0,0 +1,94 @@
+package io.android.fanito_androidclient.di
+
+
+import android.app.Application
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.fanito.android.BuildConfig
+import io.fanito.android.utils.RetryCallAdapterFactory
+import io.fanito.android.data.AppDataManager
+import io.fanito.android.data.DataManager
+import io.fanito.android.data.local.prefs.AppPreferencesHelper
+import io.fanito.android.data.local.prefs.PreferencesHelper
+import io.fanito.android.data.remote.ApiHelper
+import io.fanito.android.data.remote.ApiServices
+import io.fanito.android.data.remote.AppApiHelper
+import io.fanito.android.data.resource.AppResourceHelper
+import io.fanito.android.data.resource.ResourceHelper
+import io.fanito.android.di.annotation.DatabaseInfo
+import io.fanito.android.di.annotation.PreferenceInfo
+import io.fanito.android.utils.AppConstants
+import io.fanito.android.utils.HeaderInterceptor
+import io.fanito.android.utils.NetworkConnectionInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        networkConnectionInterceptor: NetworkConnectionInterceptor,
+        headerInterceptor: HeaderInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(networkConnectionInterceptor)
+        .addInterceptor(headerInterceptor)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideApiServices(
+        gson: Gson,
+        okHttpclient: OkHttpClient
+    ): ApiServices = Retrofit.Builder()
+        .client(okHttpclient)
+        .baseUrl(BuildConfig.BASE_URL)
+        .addCallAdapterFactory(RetryCallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+        .create(ApiServices::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiHelper(appApiHelper: AppApiHelper): ApiHelper = appApiHelper
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context = application
+
+    @Provides
+    @Singleton
+    fun provideDataManager(appDataManager: AppDataManager): DataManager = appDataManager
+
+    @Provides
+    @DatabaseInfo
+    fun provideDatabaseName(): String = AppConstants.DB_NAME
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+
+    @Provides
+    @Singleton
+    fun provideResourceHelper(appResourceHelper: AppResourceHelper): ResourceHelper =
+        appResourceHelper
+
+    @Provides
+    @PreferenceInfo
+    fun providePreferenceName(): String = AppConstants.PREF_NAME
+
+    @Provides
+    @Singleton
+    fun providePreferencesHelper(appPreferencesHelper: AppPreferencesHelper): PreferencesHelper =
+        appPreferencesHelper
+
+}
