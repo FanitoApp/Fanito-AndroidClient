aded0d1f857e14426a37c45d105434bd30d6de51
aminsato <amin.saradar@yahoo.com>
2022-05-24 13:01:03 +0330

    Create IntroActivity_GeneratedInjector.java

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/intro/IntroActivity_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package io.android.fanito_androidclient.ui.intro;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+
+@OriginatingElement(
+    topLevelClass = IntroActivity.class
+)
+@GeneratedEntryPoint
+@InstallIn(ActivityComponent.class)
+public interface IntroActivity_GeneratedInjector {
+  void injectIntroActivity(IntroActivity introActivity);
+}
