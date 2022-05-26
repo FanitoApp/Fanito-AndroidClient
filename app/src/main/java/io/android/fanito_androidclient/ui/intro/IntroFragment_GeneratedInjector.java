95777694e09be1ffb927eb6fa665ffdf48ed7dc0
aminsato <amin.saradar@yahoo.com>
2022-05-26 13:02:42 +0330

    Create IntroFragment_GeneratedInjector.java

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/intro/IntroFragment_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package io.android.fanito_androidclient.ui.intro;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+
+@OriginatingElement(
+    topLevelClass = IntroFragment.class
+)
+@GeneratedEntryPoint
+@InstallIn(FragmentComponent.class)
+public interface IntroFragment_GeneratedInjector {
+  void injectIntroFragment(IntroFragment introFragment);
+}
