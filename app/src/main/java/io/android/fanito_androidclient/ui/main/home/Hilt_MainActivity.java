321103623aade5606f1b5b22c58b14ffb9a4a8d7
aminsato <amin.saradar@yahoo.com>
2022-07-22 05:27:05 +0330

    changes functions

--- app/src/main/java/io/android/fanito_androidclient/ui/main/home/Hilt_MainActivity.java
+++ app/src/main/java/io/android/fanito_androidclient/ui/main/home/Hilt_MainActivity.java
@@ -10,8 +10,8 @@ import dagger.hilt.android.internal.lifecycle.DefaultViewModelFactories;
 import dagger.hilt.android.internal.managers.ActivityComponentManager;
 import dagger.hilt.internal.GeneratedComponentManagerHolder;
 import dagger.hilt.internal.UnsafeCasts;
-import io.fanito.android.ui.base.BaseActivity;
-import io.fanito.android.ui.base.BaseViewModel;
+import io.android.fanito_androidclient.ui.base.BaseActivity;
+import io.android.fanito_androidclient.ui.base.BaseViewModel;
 
 /**
  * A generated base class to be extended by the @dagger.hilt.android.AndroidEntryPoint annotated class. If using the Gradle plugin, this is swapped as the base class via bytecode transformation.
---
+++ app/src/main/java/io/android/fanito_androidclient/ui/main/home/HomeNavigator.kt
@@ -0,0 +1,6 @@
+package io.android.fanito_androidclient.ui.main.home
+
+import io.android.fanito_androidclient.ui.base.BaseNavigator
+
+interface HomeNavigator : BaseNavigator {
+}
