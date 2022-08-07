672e41d4ee5969dd3ca7cbf8f17ac0594ddebbce
aminsato <amin.saradar@yahoo.com>
2022-08-07 05:33:39 +0330

    edit

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/main/hunt/HuntFragment_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package io.android.fanito_androidclient.ui.main.hunt;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+
+@OriginatingElement(
+    topLevelClass = HuntFragment.class
+)
+@GeneratedEntryPoint
+@InstallIn(FragmentComponent.class)
+public interface HuntFragment_GeneratedInjector {
+  void injectHuntFragment(HuntFragment huntFragment);
+}
