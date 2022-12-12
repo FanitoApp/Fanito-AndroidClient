5f4f5a6e11ceba0fa67232dbd16a38495bea1154
aminsato <amin.saradar@yahoo.com>
2022-12-12 05:53:42 +0330

    Create SurveysViewModel_HiltModules.java

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/main/surveys/SurveysViewModel_HiltModules.java
@@ -0,0 +1,50 @@
+package io.android.fanito_androidclient.ui.main.surveys;
+
+import androidx.lifecycle.ViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.IntoSet;
+import dagger.multibindings.StringKey;
+
+@OriginatingElement(
+    topLevelClass = SurveysViewModel.class
+)
+public final class SurveysViewModel_HiltModules {
+  private SurveysViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @StringKey("io.android.fanito_androidclient.ui.main.surveys.SurveysViewModel")
+    @HiltViewModelMap
+    public abstract ViewModel binds(SurveysViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoSet
+    @HiltViewModelMap.KeySet
+    public static String provide() {
+      return "io.android.fanito_androidclient.ui.main.surveys.SurveysViewModel";
+    }
+  }
+}
