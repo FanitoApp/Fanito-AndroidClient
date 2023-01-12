88aa393e73cb08ffa5861fb4472ad07146389f2e
aminsato <amin.saradar@yahoo.com>
2023-01-12 05:59:50 +0330

    Create WalletViewModel_HiltModules.java

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/main/wallet/WalletViewModel_HiltModules.java
@@ -0,0 +1,50 @@
+package io.android.fanito_androidclient.ui.main.wallet;
+
+import androidx.lifecycle.ViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.IntoSet;
+import dagger.multibindings.StringKey;
+
+@OriginatingElement(
+    topLevelClass = WalletViewModel.class
+)
+public final class WalletViewModel_HiltModules {
+  private WalletViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @StringKey("io.android.fanito_androidclient.ui.main.wallet.WalletViewModel")
+    @HiltViewModelMap
+    public abstract ViewModel binds(WalletViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoSet
+    @HiltViewModelMap.KeySet
+    public static String provide() {
+      return "io.android.fanito_androidclient.ui.main.wallet.WalletViewModel";
+    }
+  }
+}
