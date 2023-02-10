655640290186c0550a75e23110c6e5286c47af08
aminsato <amin.saradar@yahoo.com>
2023-02-10 10:00:48 +0330

    change activities

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/sample/activity/SampleActivityViewModel_HiltModules.java
@@ -0,0 +1,50 @@
+package io.android.fanito_androidclient.ui.sample.activity;
+
+import androidx.lifecycle.ViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.IntoSet;
+import dagger.multibindings.StringKey;
+
+@OriginatingElement(
+    topLevelClass = SampleActivityViewModel.class
+)
+public final class SampleActivityViewModel_HiltModules {
+  private SampleActivityViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @StringKey("io.android.fanito_androidclient.ui.sample.activity.SampleActivityViewModel")
+    @HiltViewModelMap
+    public abstract ViewModel binds(SampleActivityViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoSet
+    @HiltViewModelMap.KeySet
+    public static String provide() {
+      return "io.android.fanito_androidclient.ui.sample.activity.SampleActivityViewModel";
+    }
+  }
+}
