1729fd96c6b2393489e70854d8f06b07a650bf00
aminsato <amin.saradar@yahoo.com>
2023-02-22 10:06:32 +0330

    changes

---
+++ app/src/main/java/io/android/fanito_androidclient/ui/sample/fragment/SampleFragmentNavigator.kt
@@ -0,0 +1,6 @@
+package io.android.fanito_androidclient.ui.sample.fragment
+
+import io.android.fanito_androidclient.ui.base.BaseNavigator
+
+interface SampleFragmentNavigator : BaseNavigator {
+}
---
+++ app/src/main/java/io/android/fanito_androidclient/ui/sample/fragment/SampleFragmentViewModel_HiltModules.java
@@ -0,0 +1,50 @@
+package io.android.fanito_androidclient.ui.sample.fragment;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.IntoSet;
+import dagger.multibindings.StringKey;import io.android.fanito_androidclient.ui.sample.fragment.SampleFragmentViewModel;
+import java.lang.String;
+
+@OriginatingElement(
+    topLevelClass = SampleFragmentViewModel.class
+)
+public final class SampleFragmentViewModel_HiltModules {
+  private SampleFragmentViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @StringKey("io.android.fanito_androidclient.ui.sample.fragment.SampleFragmentViewModel")
+    @HiltViewModelMap
+    public abstract ViewModel binds(SampleFragmentViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoSet
+    @HiltViewModelMap.KeySet
+    public static String provide() {
+      return "io.android.fanito_androidclient.ui.sample.fragment.SampleFragmentViewModel";
+    }
+  }
+}
